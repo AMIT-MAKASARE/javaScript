14749b1e92073b60109f1078b128b7c7a65ae24c
AMIT-MAKASARE <amitmakasare747@gmail.com>
2024-10-02 04:02:51 +0000

    js basics file add

---
+++ DataTypeConversion/index.js
@@ -0,0 +1,21 @@
+// let score="false"
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let inNum=Number(score)
+// console.log(typeof inNum);
+// console.log(inNum);
+
+
+// this above code is for the covert string to number
+// "11"=> 33 okye
+// "11abc"=> NaN
+
+let convert="0"
+let inBoolean=Boolean(convert)
+console.log(inBoolean);
+
+// true =>1, false=>0
+// ""=> false
+// "amit"=> true
+
---
+++ DataTypes/DT.js
@@ -0,0 +1,16 @@
+console.log("all datatypes given below..");
+
+// string
+let name="amit"; 
+// number
+let id=21;
+// Boolean
+let studentPresent=true;
+// undefined
+let student;
+// null
+let teacher=null
+
+console.log(typeof studentPresent);    // data type of undefined is undefined 
+console.log(typeof null);       //data type of null is object 
+
