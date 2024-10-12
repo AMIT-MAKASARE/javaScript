89e3e7e402de6f578bb75b049cc6f2bab726a693
AMIT-MAKASARE <amitmakasare747@gmail.com>
2024-10-12 17:44:14 +0000

    add basic date functions

---
+++ date/main.js
@@ -0,0 +1,28 @@
+let dates=new Date()
+// console.log(dates.toString());  date month year time everything
+// console.log(dates.toISOString()); year month date time, all in number
+// console.log(dates.toUTCString());    sat,date month year ,GMT time all num
+// console.log(dates.toDateString());    date month year only
+// console.log(dates.toLocaleDateString());  proper date/month/year
+// console.log(dates.toLocaleString());      proper date/month/year with time=> hour,min,sec pm
+// console.log(dates.toLocaleTimeString());     only time => hour,min,sec pm
+// console.log(dates.toJSON());       year-month-date and something more...
+
+// console.log(typeof dates);  data type of the Date() is Object
+
+
+// let createDate=new Date(2024,0,25)   here 0 means the first month, jan
+
+// month date year ,hour min sec
+let createDate=new Date( 2024,4,12,4,5)  
+console.log(createDate.toLocaleString());
+
+// <----time Stamps------->
+
+
+
+
+
+
+
+
