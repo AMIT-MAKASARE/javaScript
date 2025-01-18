b062e5d26ffbf306ceb4a2c1f2752728a7832937
AMIT-MAKASARE <amitmakasare747@gmail.com>
2025-01-18 06:58:49 +0000

    set interval questions

--- setTimeOutSetInterval/main.js
+++ setTimeOutSetInterval/main.js
@@ -1,8 +1,27 @@
-console.log("hello there");
+// console.log("hello there");
 
-setInterval(() => {
-    console.log("amit makasare");
+// setInterval(() => {
+//     console.log("amit makasare");
 
-}, );
+// }, );
 
-console.log("i am ")
+// console.log("i am ")
+
+
+// squre using arrow fuction
+let sqr=(n)=>{
+    return n*n;  
+}
+console.log(sqr(33))
+
+
+// funciton who print 5times hellow world using setInterval
+
+
+   let id= setInterval(()=>{
+        console.log("hellow world");
+    },2000)
+
+    setTimeout(() => {
+        clearInterval(id)
+    }, 10000);
